22d18f09e4137278c020e02df32847e15e082750
Mujahid-Developer <mujahidulislam575@gmail.com>
2021-09-22 14:17:51 +0600

    new card dcesign

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,42 +1,22 @@
 import React from 'react';
-import { img_300 } from '../../config/config';
+import { img_300, unavailable } from '../../config/config';
 import './MovieCard.css'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+
 
 const MovieCard = (props) => {
-    const { original_title, poster_path, vote_average, overview } = props.movie;
+    const { original_title, original_name, poster_path, vote_average, media_type } = props.movie;
     console.log(props);
 
     return (
         <div className="movie">
-            <Card sx={{
-                maxWidth: 350,
-                margin: '10px'
-            }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="500"
-                        image={`${img_300}/${poster_path}`}
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {original_title}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {overview.slice(0, 200)}
-                        </Typography>
-                        <Typography variant="body2" color="text.danger">
-                            Rating {vote_average}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
+            <div className="movie-thumb shadow-sm p-2 mb-3">
+                <img src={poster_path ? `${img_300}/${poster_path}` : unavailable} alt="" srcset="" />
+                <h5 className="text-center">{original_title ? original_title : original_name}</h5>
+                <div className="movie-buttom mt-3">
+                    <span>Categories: {media_type === 'tv' ? "Tv Series" : "Movie"}</span> <br />
+                    <span>Rating: {vote_average}</span>
+                </div>
+            </div>
         </div>
     );
 };
