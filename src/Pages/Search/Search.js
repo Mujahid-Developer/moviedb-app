c623482430316c069e2d00ebdbfa4317f0b1585a
Mujahid-Developer <mujahidulislam575@gmail.com>
2021-09-25 20:36:03 +0600

    adding Series

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { Button, createMuiTheme, Tab, Tabs, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from '@mui/system';
+
 
 const Search = () => {
+    const [type, setType] = useState(0)
+
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                main: '#1b1b1b',
+            },
+            secondary: {
+                main: '#edf2ff',
+            },
+        },
+    });
+
     return (
         <div>
-            <span className="page-title">Search</span>
+            <ThemeProvider theme={theme}>
+                <div style={{ display: "flex", width: '100%', marginTop: '70px' }}>
+                    <TextField
+                        style={{ flex: 1, marginRight: '10px' }}
+                        className="searchBox"
+                        label="Search Here"
+                        variant="filled"
+                    />
+                    <Button variant="contained"> <SearchIcon /> </Button>
+                </div>
+                <Tabs value={type} indicatorColor="primary" textColor="primary" style={{ marginTop: '10px' }} >
+                    <Tab style={{ width: '50%' }} label="Search Movies" />
+                    <Tab style={{ width: '50%' }} label="Search Series" />
+                </Tabs>
+            </ThemeProvider>
         </div>
     );
 };
--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import MovieCard from '../../components/MovieCard/MovieCard';
+import CustomPagination from '../../components/Pagination/CustomPagination';
+import Genres from '../../components/Genres/Genres';
+import useGenres from '../../hooks/useGenres';
 
 const Series = () => {
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([])
+    const [numberOfPages, setNumberOfPages] = useState()
+    const [selectedGenres, setSelectedGenres] = useState([])
+    const [genres, setGenres] = useState([])
+    const genreforURL = useGenres(selectedGenres);
+
+    const url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+
+    const fetchMovies = async () => {
+        const { data } = await axios.get(url)
+        setContent(data.results)
+        setNumberOfPages(data.total_pages)
+    }
+    useEffect(() => {
+        fetchMovies();
+        // eslint-disable-next-line
+    }, [page, genreforURL])
     return (
         <div>
-            <span className="page-title">Series</span>
+            <h1 className="text-center headings"> Discover Series</h1>
+            <Genres
+                type="tv"
+                selectedGenres={selectedGenres}
+                setSelectedGenres={setSelectedGenres}
+                genres={genres}
+                setGenres={setGenres}
+                setPage={setPage}
+            />
+            <div className="movie-card">
+                {
+                    content.map((item) => <MovieCard movie={item}></MovieCard>)
+                }
+            </div>
+            {numberOfPages > 1 &&
+                (<CustomPagination setPage={setPage} numberOfPages={numberOfPages} />)}
         </div>
     );
 };
